3e490fa243f1725fe6a42d3172a9bc5e87a68f47
xcode <xcode@xcode.com>
2025-01-22 11:45:54 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-def add(a,b):
+def add(a, b):
     return a+b
 
+def sub(a, b):
+    return a-b
 
+result1 = add(10, 20)
+result2 = sub(20, 10)
 
-
-def sub(a,b):return a-b
-
-result1 = add(10,20)
-result2 = sub(20,10)
-
-print(result1,result2)
+print(result1, result2)
