3e6bc27134e2bb6fe602015413512c183592a580
xcode <xcode@xcode.com>
2025-01-23 00:48:35 +0530

    Add Multiply Function

--- app.py
+++ app.py
@@ -13,5 +13,7 @@ def mul(a, b):
 if __name__ == "__main__":
     result1 = add(4, 5)
     result2 = sub(4, 1)
+    result3 = mul(4, 5)
 
-    print(result1, result2)
+    print(result1, result2, result3)
+
